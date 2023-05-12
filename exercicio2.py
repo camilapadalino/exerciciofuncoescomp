9e472f9b92e31258087a1c77067e776bcc417c45
Logon Aluno <logonrmlocal@fiap.com.br>
2023-05-12 11:36:53 -0300

    subindo ex2

--- exercicio2.py
+++ exercicio2.py
@@ -5,5 +5,28 @@ Por exemplo: 120 é triangular, pois 4 * 5 * 6 = 120.
 Faça uma função que receba um número inteiro e retorne True se for um número triangular e False,
 caso contrário.
 """
-def sequencia():
-    if
+def triangulo(numero):
+    if type(numero) != int or numero < 0:
+        raise TypeError("O número deve ser natural")
+    
+    for a in range(numero):
+        if  a * (a + 1) * (a + 2) == numero:
+            return True
+        
+    return False
+
+controle = True
+while controle:
+    while True:
+        try:
+            numero = int(input("Insira um número inteiro: "))
+            if triangulo(numero) == True:
+                print(f'O número {numero} é triangular.')
+            else:
+                print(f'O número {numero} não é triangular.')
+        except ValueError:
+            print('ERRO: o valor informado não é inteiro.')
+        except TypeError as erro:
+            print(f'ERRO:{erro}')
+        else:
+            break
